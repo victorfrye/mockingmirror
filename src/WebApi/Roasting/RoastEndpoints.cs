4b2d7d2d6cbe03c32ae962554708b5ca12092e34
Victor Frye <victorfrye@outlook.com>
2025-05-16 21:09:51 -0400

    chore: it works

--- src/WebApi/Roasting/RoastEndpoints.cs
+++ src/WebApi/Roasting/RoastEndpoints.cs
@@ -4,15 +4,12 @@ internal static class RoastEndpoints
 {
     internal static void MapRoastingEndpoints(this WebApplication app)
     {
-        app.MapPostRoastEndpoint();
+        app.MapPost("/roasts", PostRoast);
     }
 
-    private static void MapPostRoastEndpoint(this WebApplication app)
+    internal static async Task<IResult> PostRoast(IRoastService service, RoastRequest request, CancellationToken cancellationToken)
     {
-        app.MapPost("/roasts", async (IRoastService service, RoastRequest request, CancellationToken cancellationToken) =>
-        {
-            var response = await service.AddRoast(request, cancellationToken);
-            return Results.Created($"/roasts/{response.Id}", response);
-        });
+        var response = await service.AddRoast(request, cancellationToken);
+        return Results.Created($"/roasts/{response.Id}", response);
     }
 }
